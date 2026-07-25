fffb8000c50737a65f6cc1f635c22590da5833e0
rkyugabd <rkyugabd@gmail.com>
2026-07-25 19:25:06 +0200

    Connect frontend to Render backend

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     try {
 
       const response = await fetch(
-        "http://127.0.0.1:8000/generate-report",
+         "https://ai-manufacturing-design-assistant.onrender.com/generate-report",
         {
           method: "POST",
 
